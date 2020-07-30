04868caecf357d2b2079f5773731a553043dc36c
James Asbury <jamesasbury03@gmail.com>
2020-07-30 16:16:28 -0700

    Comments and fixes. New license

--- parser.go
+++ parser.go
@@ -1,69 +1,66 @@
-// Parse lines and pipelines
+// Parse lines and pipelines. See LICENSE
 package goshell
 
 import (
-	"fmt"
 	"errors"
-	"strings"
+	"fmt"
 	"os"
-	"syscall"
 	"os/exec"
+	"strings"
+	"syscall"
 )
 
 const (
-	inRedirectChar string = "<"
+	inRedirectChar  string = "<"
 	outRedirectChar string = ">"
-	pipeChar string = "|"
+	pipeChar        string = "|"
 )
 
-func ProcessLine(cmdLine string) {
+// Process a line of input and execute it
+func processLine(cmdLine string) {
 	cmdLine = strings.TrimSpace(cmdLine)
-	
+
 	commandList := make([]exec.Cmd, 0)
 
 	if len(cmdLine) == 0 {
-		return;
+		return
 	}
 
 	pipeStages := strings.Split(cmdLine, pipeChar)
-	
-	err := CreatePipeStages(&commandList, pipeStages)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "%v: %v.\n", shellName, err) 
-	}
 
+	err := createPipeStages(&commandList, pipeStages)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "%v: %v.\n", shellName, err)
 		return
 	}
-	
-	err = ConnectPipeline(commandList)
+
+	err = connectPipeline(commandList)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "%v: Error with pipes: %v.\n", shellName, err)
 		return
 	}
 
-	err = ExecutePipeline(commandList)
+	err = executePipeline(commandList)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "%v: Error during execution: %v\n", shellName, err)
 		return
 	}
 }
 
-func CreatePipeStages(commandList *[]exec.Cmd, stages []string) error {
-	
+// Separate out a string slice of pipeline of commands into Cmd slice
+func createPipeStages(commandList *[]exec.Cmd, stages []string) error {
+
 	for _, stage := range stages {
 		cmd, err := CreateCommand(stage)
 		if err != nil {
 			return err
 		}
 		*commandList = append(*commandList, *cmd)
-
 	}
-
 	return nil
 }
 
+// Creates a command using a string of one pipeline step
 func CreateCommand(pipeStage string) (*exec.Cmd, error) {
 	args := strings.Fields(pipeStage)
 	executionArgs := make([]string, 0)
@@ -76,6 +73,7 @@ func CreateCommand(pipeStage string) (*exec.Cmd, error) {
 
 	var inRedirectFile, outRedirectFile string
 
+	// Any redirection specifiers (</>) will get parsed and saved
 	i := 0
 	for i < len(args) {
 		if strings.HasPrefix(args[i], inRedirectChar) {
@@ -88,10 +86,10 @@ func CreateCommand(pipeStage string) (*exec.Cmd, error) {
 			} else if i == (len(args) - 1) {
 				return nil, errors.New("Redirection must include input file name")
 			} else {
-				inRedirectFile = args[i + 1]
+				inRedirectFile = args[i+1]
 				i += 2
 			}
-		} else if strings.HasPrefix(args[i], outRedirectChar){
+		} else if strings.HasPrefix(args[i], outRedirectChar) {
 			if i == 0 {
 				return nil, errors.New("Command must preceed output redirection")
 			}
@@ -101,18 +99,20 @@ func CreateCommand(pipeStage string) (*exec.Cmd, error) {
 			} else if i == (len(args) - 1) {
 				return nil, errors.New("Redirection must include output file name")
 			} else {
-				outRedirectFile = args[i + 1]
+				outRedirectFile = args[i+1]
 				i += 2
 			}
 		} else if i < len(args) {
+			// Save command arguments only if they arent references to redirection
 			executionArgs = append(executionArgs, args[i])
 			i++
 		}
 	}
+	// Create a command using only the command name and arguments
 	cmd = exec.Command(executionArgs[0], executionArgs[1:]...)
-	
+
+	// Set the redirect output files from the data we parsed
 	err := setRedirects(cmd, inRedirectFile, outRedirectFile)
-	
 	if err != nil {
 		return nil, err
 	}
@@ -120,8 +120,10 @@ func CreateCommand(pipeStage string) (*exec.Cmd, error) {
 	return cmd, nil
 }
 
+// Takes in strings of any files for redirection. Opens them and associates
+// them with the Cmd object as a File* (Fd)
 func setRedirects(cmd *exec.Cmd, inFile string, outFile string) error {
-	if inFile != ""{
+	if inFile != "" {
 		inF, err := os.OpenFile(inFile, syscall.O_RDONLY, 0666)
 		if err != nil {
 			return err
@@ -138,17 +140,18 @@ func setRedirects(cmd *exec.Cmd, inFile string, outFile string) error {
 	return nil
 }
 
-func ConnectPipeline(commandList []exec.Cmd) error {
+// Connect the commands together as a variable length pipeline
+func connectPipeline(commandList []exec.Cmd) error {
 	var err error
 
 	for i, _ := range commandList {
-		if i == len(commandList) - 1 {
+		if i == len(commandList)-1 {
 			break
 		}
-		if(commandList[i + 1].Stdin != nil) {
+		if commandList[i+1].Stdin != nil || commandList[i].Stdout != nil {
 			return errors.New("Ambiguous input for file redirection and pipe")
 		}
-		commandList[i + 1].Stdin, err = commandList[i].StdoutPipe()
+		commandList[i+1].Stdin, err = commandList[i].StdoutPipe()
 		if err != nil {
 			return err
 		}
@@ -156,13 +159,14 @@ func ConnectPipeline(commandList []exec.Cmd) error {
 	if commandList[0].Stdin == nil {
 		commandList[0].Stdin = os.Stdin
 	}
-	if commandList[len(commandList) - 1].Stdout == nil {
-		commandList[len(commandList) - 1].Stdout = os.Stdout
+	if commandList[len(commandList)-1].Stdout == nil {
+		commandList[len(commandList)-1].Stdout = os.Stdout
 	}
 	return nil
 }
 
-func ExecutePipeline(commandList []exec.Cmd) error {
+// Execute all commands. Pipeline commands will wait on the previous command
+func executePipeline(commandList []exec.Cmd) error {
 	//TODO: Handle case where the command is built in
 	// Start execution in reverse pipe order
 	for i := len(commandList) - 1; i > 0; i-- {
@@ -170,7 +174,7 @@ func ExecutePipeline(commandList []exec.Cmd) error {
 	}
 
 	// Make the commands wait for completion
-	for i,_ := range commandList {
+	for i, _ := range commandList {
 		if i == 0 {
 			err := commandList[i].Run()
 			if err != nil {
--- shell.go
+++ shell.go
@@ -1,64 +1,75 @@
-// The main driver and shell management
+// The main driver and shell management. See LICENSE
 package goshell
 
 import (
-   "fmt"
-   "os"
-   "bufio"
-   "runtime"
+	"bufio"
+	"fmt"
+	"os"
+	"runtime"
 )
 
 var shellName = "Go Shell"
 var prompt = "G$ "
 var startMessage = "Welcome to Go Shell! The shell coded entirely in Go(lang)\n"
 
-var Reset   = "\033[0m"
-var Red     = "\033[31m"
-var Green   = "\033[32m"
-var Yellow  = "\033[33m"
-var Blue    = "\033[34m"
-var Purple  = "\033[35m"
-var Cyan    = "\033[36m"
-var Gray    = "\033[37m"
-var White   = "\033[97m"
+var Reset = "\033[0m"
+var Red = "\033[31m"
+var Green = "\033[32m"
+var Yellow = "\033[33m"
+var Blue = "\033[34m"
+var Purple = "\033[35m"
+var Cyan = "\033[36m"
+var Gray = "\033[37m"
+var White = "\033[97m"
 
+// The run loop. Use for starting the shell
 func Run() {
-   if runtime.GOOS == "windows" {
+	// This shell is currently Unix only
+	if runtime.GOOS == "windows" {
 		fmt.Fprintf(os.Stderr, "%s: Sorry - currently unavailable for Windows\n", shellName)
+		return
 	}
-   fmt.Println(startMessage)
 
-   lineReader := bufio.NewReader(os.Stdin)
-   for {
-      fmt.Printf(Green + "%s" + Reset , prompt)
-      cmdLine, err := lineReader.ReadString('\n')
-      if err != nil {
-         fmt.Fprintln(os.Stderr, err)
-      }
-      ProcessLine(cmdLine)
-   }
+	// Display a start message to the user
+	fmt.Println(Green + startMessage + Reset)
+
+	lineReader := bufio.NewReader(os.Stdin)
+
+	// The main loop of requesting input and processing it
+	for {
+		fmt.Printf(Green+"%s"+Reset, prompt)
+		cmdLine, err := lineReader.ReadString('\n')
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+		}
+		processLine(cmdLine)
+	}
 }
 
+// Change the input prompter to a new string
 func SetPrompt(newPrompt string) {
-   prompt = newPrompt
+	prompt = newPrompt
 }
 
+// Change the welcome message to a new string
 func SetStartMessage(newMessage string) {
-   startMessage = newMessage
+	startMessage = newMessage
 }
 
+// Set the name of the shell. Used in debugging
 func SetShellName(newName string) {
-   shellName = newName
+	shellName = newName
 }
 
+// Disable the use of special colors
 func DisableColors() {
-   Reset   = ""
-   Red     = ""
-   Green   = ""
-   Yellow  = ""
-   Blue    = ""
-   Purple  = ""
-   Cyan    = ""
-   Gray    = ""
-   White   = ""
+	Reset = ""
+	Red = ""
+	Green = ""
+	Yellow = ""
+	Blue = ""
+	Purple = ""
+	Cyan = ""
+	Gray = ""
+	White = ""
 }
